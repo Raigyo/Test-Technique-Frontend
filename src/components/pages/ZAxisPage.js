369d32864a186a3194cdc38281263802e3e0da4d
Raigyo <vincent.chilot@gmail.com>
2019-06-15 20:49:25 +0200

    slider wip

--- src/components/pages/ZAxisPage.js
+++ src/components/pages/ZAxisPage.js
@@ -32,14 +32,61 @@ export default (props) => {
     height: '120px'
   });
   const blockSpeed = css({height: '120px'});
-  const blockRotate = css({height: '120px', margin: '23px 15px 0 15px'});
-  const blockOtherControls = css({height: '120px', position: 'relative'});
+  const blockRotate = css({
+    height: '120px',
+    margin: '23px 15px 0 15px'
+  });
+  const blockOtherControls = css({
+    height: '120px',
+    position: 'relative'
+  });
   //Control buttons
-  const btnControl = css({width: '47px', height: '40px'});
-  const btnControlHome = css({position: 'absolute', bottom: '0'});
-  const btnArrows = css({width: '13px', margin: 'auto', display: 'inlineBlock', height: '100%', verticalAlign: 'middle'});
-  const btnArrowsFrameTop = css({width: '112px', height: '47px', backgroundColor: '#445A64', borderRadius: '10% 10% 0 0', margin: '0 0 1px 0'});
-  const btnArrowsFrameBottom = css({width: '112px', height: '47px', backgroundColor: '#445A64', borderRadius: '0 0 10% 10%'});
+  const btnControl = css({
+    width: '47px',
+    height: '40px'
+  });
+  const btnControlHome = css({
+    position: 'absolute',
+    bottom: '0'
+  });
+  const btnArrows = css({
+    width: '13px',
+    margin: 'auto',
+    display: 'inlineBlock',
+    height: '100%',
+    verticalAlign: 'middle'
+  });
+  const btnArrowsFrameTop = css({
+    width: '112px',
+    height: '47px',
+    backgroundColor: '#445A64',
+    borderRadius: '10% 10% 0 0',
+    margin: '0 0 1px 0'
+  });
+  const btnArrowsFrameBottom = css({
+    width: '112px',
+    height: '47px',
+    backgroundColor: '#445A64',
+    borderRadius: '0 0 10% 10%'
+  });
+  //Slider
+  const blockSlider = css({
+    position: 'fixed!important',
+    right: '0',
+    top: '50%',
+    width: '8em',
+    marginTop: '-2.5em'
+  });
+  const layoutSlider = css({
+    position: 'relative',
+    width: '365px',
+    height: '8px',
+    borderRadius: '5px',
+    background: '#d3d3d3',
+    outline: 'none',
+    opacity: '0.7'
+  });
+
   // Hook: Similar to componentDidMount and componentDidUpdate, used to rerender toggle icons
   const [isSelected, setBtnIdRadio] = useState(0);
   const [btnId, setBtnIdOver] = useState(0);
@@ -47,44 +94,47 @@ export default (props) => {
 
   return (
     <Page>
-    <Page.Header>
-      Z Axis
-      <Page.LeftButton icon={<Home/>} to='/control'/>
-    </Page.Header>
-    <Page.Body className={styleBody}>
-        {/*Conditional Ternary Operator Rendering for toggle icons*/}
-      <div className={blockCenter}>
-        <div className={blockSpeed}>
-          {isSelected==1 ? (<div className={btnControl}><SelectedFast/></div>) : (<div className={btnControl} onClick={() => setBtnIdRadio(1)}><ButtonFast/></div>)}
-          {isSelected==2 ? (<div className={btnControl}><SelectedNormal/></div>) : (<div className={btnControl} onClick={() => setBtnIdRadio(2)}><ButtonNormal/></div>)}
-          {isSelected==3 ? (<div className={btnControl}><SelectedSlow/></div>) : (<div className={btnControl} onClick={() => setBtnIdRadio(3)}><ButtonSlow/></div>)}
-        </div>
-        <div className={blockRotate}>
-          <div className={btnArrowsFrameTop}><div className={btnArrows}><ButtonUP/></div></div>
-          <div className={btnArrowsFrameBottom}><div className={btnArrows}><ButtonDown/></div></div>
-        </div>
-        <div className={blockOtherControls}>
-          {btnId ? (<div className={btnControl} onMouseOut={() => setBtnIdOver(1)}><ButtonStopHover/></div>) : (<div className={btnControl} onClick={() => console.log('!stop!')} onMouseOver={() => setBtnIdOver(1)}><ButtonStop/></div>)}
-          {btnId ? (<div className={btnControl, btnControlHome} onMouseOut={() => setBtnIdOver(2)}><ButtonStartPositionHover/></div>) : (<div className={btnControl, btnControlHome} onClick={() => console.log('home')} onMouseOver={() => setBtnIdOver(2)}><ButtonStartPosition/></div>)}
-        </div>
-      </div>
-    </Page.Body>
-
-      {/*<Page.Header>
+      <Page.Header>
         Z Axis
         <Page.LeftButton icon={<Home/>} to='/control'/>
       </Page.Header>
       <Page.Body className={styleBody}>
-        <SelectedFast/><ButtonFast/>
-        <SelectedNormal/><ButtonSlow/>
-        <SelectedSlow/><ButtonNormal/>
-        <ButtonUP/>
-        <ButtonDown/>
-        <ButtonStop/>
-        <ButtonStopHover/>
-        <ButtonStartPosition/>
-        <ButtonStartPositionHover/>
-      </Page.Body>*/}
+          {/*Conditional Ternary Operator Rendering for toggle icons*/}
+        <div className={blockCenter}>
+          <div className={blockSpeed}>
+            {isSelected==1 ? (<div className={btnControl}><SelectedFast/></div>) : (<div className={btnControl} onClick={() => setBtnIdRadio(1)}><ButtonFast/></div>)}
+            {isSelected==2 ? (<div className={btnControl}><SelectedNormal/></div>) : (<div className={btnControl} onClick={() => setBtnIdRadio(2)}><ButtonNormal/></div>)}
+            {isSelected==3 ? (<div className={btnControl}><SelectedSlow/></div>) : (<div className={btnControl} onClick={() => setBtnIdRadio(3)}><ButtonSlow/></div>)}
+          </div>
+          <div className={blockRotate}>
+            <div className={btnArrowsFrameTop}><div className={btnArrows}><ButtonUP/></div></div>
+            <div className={btnArrowsFrameBottom}><div className={btnArrows}><ButtonDown/></div></div>
+          </div>
+          <div className={blockOtherControls}>
+            {btnId ? (<div className={btnControl} onMouseOut={() => setBtnIdOver(1)}><ButtonStopHover/></div>) : (<div className={btnControl} onClick={() => console.log('!stop!')} onMouseOver={() => setBtnIdOver(1)}><ButtonStop/></div>)}
+            {btnId ? (<div className={btnControl, btnControlHome} onMouseOut={() => setBtnIdOver(2)}><ButtonStartPositionHover/></div>) : (<div className={btnControl, btnControlHome} onClick={() => console.log('home')} onMouseOver={() => setBtnIdOver(2)}><ButtonStartPosition/></div>)}
+          </div>
+        </div>
+        <div className="blockSlider">
+          <input className={layoutSlider} id="layoutSlider" type="range" min="0.001" max="50" value="0.001" />
+        </div>
+      </Page.Body>
+
+        {/*<Page.Header>
+          Z Axis
+          <Page.LeftButton icon={<Home/>} to='/control'/>
+        </Page.Header>
+        <Page.Body className={styleBody}>
+          <SelectedFast/><ButtonFast/>
+          <SelectedNormal/><ButtonSlow/>
+          <SelectedSlow/><ButtonNormal/>
+          <ButtonUP/>
+          <ButtonDown/>
+          <ButtonStop/>
+          <ButtonStopHover/>
+          <ButtonStartPosition/>
+          <ButtonStartPositionHover/>
+        </Page.Body>*/}
     </Page>
   );
 };
